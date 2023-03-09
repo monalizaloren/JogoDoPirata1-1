1b75725d7c420cd4394c94da00c4ab6170d615c8
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-09 07:30:26 -0300

    Update Cannon.js

--- js/Cannon.js
+++ js/Cannon.js
@@ -10,15 +10,6 @@ class Cannon {
     this.cannon_base = loadImage("assets/cannonBase.png");
   }
   display() {
-    /*funções push() e pop() são usadas para armazenar e restaurar a matriz de transformação atual. Isso é feito para que a rotação e a translação aplicadas ao canhão não afetem outros elementos na tela.*/
-    push();
-    translate(this.x, this.y);
-    rotate(this.angle);
-    /*A função imageMode() é usada para definir o modo de desenho da imagem. Nesse caso, o modo é definido como CENTER, o que significa que a imagem é desenhada a partir do seu centro.*/
-    imageMode(CENTER);
-    image(this.cannon_image, 0, 0, this.width, this.height);
-    pop();
-    image(this.cannon_base,70, 20, 200, 200);
     
   }
 }
